44913a7de58f01698c3b28837da1675bd25289e7
test <test@gmail.com>
2023-10-13 01:07:12 +0300

    Убрал из burger.js функцию для follow и account + небольшой рефак в app.js + скролл в пагинаторе

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,6 @@
 (function($){
 	$(document).ready(function(){ 
 
-		$('.scroll-down').on('click', function(){
-			$('html, body').animate({
-				scrollTop: ($('.content').offset().top - 300)
-			}, 900, function(){ 
-				//console.log('callback function'); 
-			});
-		});
-
 		$(window).on('scroll load', function(e){
 			if(pageYOffset > $('.header').outerHeight(true)){
 				$('.top-line').addClass('fixed');
@@ -18,6 +10,52 @@
 				$('body').removeClass('top-line-fixed');
 			}
 		});
+
+		$(window).on('load resize', function(){
+
+			let wW = $(window).innerWidth();
+			let account = $('.account');
+			let follow  = $('.follow-us');
+			let menu    = $('.header-menu');
+			let box     = $('.header-title-box');
+
+			function addClassInner(element, menu, box){
+				if( wW <= 767 && !(element.hasClass('inner')) ){
+					element.appendTo(menu);
+					element.addClass('inner');
+				} else if( wW > 767 && element.hasClass('inner') ) {
+					if(box){
+						box.after(element);
+						element.removeClass('inner');
+					} else {
+						menu.after(element);
+						element.removeClass('inner');
+					}
+					
+				}
+			}
+
+			addClassInner(account, menu);
+			addClassInner(follow, menu, box);
+			
+		});
+
+		function scrollTopAnimate(trigger, target, offset, speed){
+
+			trigger.on('click', function(){
+
+				$(this).attr('id') ? target = $('.' + $(this).attr('id')) : true;
+
+				$('html, body').animate({
+					scrollTop: target.offset().top - offset
+				}, speed, function(){ 
+					//console.log('callback function'); 
+				});
+			});
+		}
+
+		scrollTopAnimate($('.scroll-down'), $('.content'), 300, 900);
+		scrollTopAnimate($('.pagination__element'), true, 150, 700);
 
 		$('.header-menu__link').on('click', function(){
 			$(this).addClass('active').siblings().removeClass('active');
--- app/js/burger.js
+++ app/js/burger.js
@@ -10,30 +10,5 @@
 			$('body').toggleClass('lock');
 		});
 
-		$(window).on('load resize', function(){
-
-			let wW = $(window).innerWidth();
-			let account = $('.account');
-			let follow  = $('.follow-us');
-			let menu    = $('.header-menu');
-			let box     = $('.header-title-box');
-
-			if( wW <= 767 && !(account.hasClass('inner')) ){
-				account.appendTo(menu);
-				account.addClass('inner');
-			} else if( wW > 767 && account.hasClass('inner') ) {
-				menu.after(account);
-				account.removeClass('inner');
-			}
-
-			if( wW <= 767 && !(follow.hasClass('inner')) ){
-				follow.appendTo(menu);
-				follow.addClass('inner');
-			} else if( wW > 767 && follow.hasClass('inner') ) {
-				box.after(follow);
-				follow.removeClass('inner');
-			}
-		});
-
 	});
 })(jQuery);
